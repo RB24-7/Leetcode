568c7813ded33e1e1d2b80f5ba540105565d4ee7
JP <jorge_pompa@ymail.com>
2025-01-13 23:56:12 -0800

    plusOne.cpp

---
+++ plusOne.cpp
@@ -0,0 +1,17 @@
+ vector<int> plusOne(vector<int>& digits) 
+ {
+    int sizeOfDigits = digits.size();
+
+    for(int i = sizeOfDigits - 1; i >= 0; i--) 
+    {
+        if(digits[i] < 9)
+        {
+            digits[i]++;
+            return digits;
+        }
+            digits[i] = 0;
+    }
+    
+    digits.insert(digits.begin(), 1);
+    return digits;
+}
